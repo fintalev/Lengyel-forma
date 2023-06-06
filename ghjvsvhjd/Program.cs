6b4cc6af92d1df72dabb7254b2505cf49bbbd870
ny19fintal <ny19fintal@201-10>
2023-06-06 12:03:20 +0200

    Add project files.

---
+++ ghjvsvhjd/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        string infixExpression =Console.ReadLine();
+        string postfixExpression = ConvertToPostfix(infixExpression);
+        Console.WriteLine("Lengyel forma: " + postfixExpression);
+
+        Console.ReadKey();
+    }
+
+    public static string ConvertToPostfix(string infixExpression)
+    {
+        Dictionary<char, int> precedence = new Dictionary<char, int>();
+        precedence.Add('+', 1);
+        precedence.Add('-', 1);
+        precedence.Add('*', 2);
+        precedence.Add('/', 2);
+        precedence.Add('^', 3);
+
+        Stack<char> operators = new Stack<char>();
+        string postfix = "";
+
+        foreach (char c in infixExpression)
+        {
+            if (char.IsDigit(c))
+            {
+                postfix += c;
+            }
+            else if (c == '(')
+            {
+                operators.Push(c);
+            }
+            else if (c == ')')
+            {
+                while (operators.Count > 0 && operators.Peek() != '(')
+                {
+                    postfix += operators.Pop();
+                }
+
+                operators.Pop();
+            }
+            else if (precedence.ContainsKey(c))
+            {
+                while (operators.Count > 0 && operators.Peek() != '(' && precedence[c] <= precedence[operators.Peek()])
+                {
+                    postfix += operators.Pop();
+                }
+
+                operators.Push(c);
+            }
+        }
+
+        while (operators.Count > 0)
+        {
+            postfix += operators.Pop();
+        }
+
+        return postfix;
+    }
+}
